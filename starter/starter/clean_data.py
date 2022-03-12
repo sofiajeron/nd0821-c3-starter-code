145279307b0b889021995a378400ab4319d666ee
sofiajeron <sofiajdossantos@gmail.com>
2022-03-12 22:33:05 +0000

    change paths

--- starter/starter/clean_data.py
+++ starter/starter/clean_data.py
@@ -3,10 +3,10 @@ from ml.clean import clean_column_names
 
 
 # Add code to load in the data.
-data = pd.read_csv('../data/census.csv')
+data = pd.read_csv('data/census.csv')
 
 #clean_data
 data = clean_column_names(data)
 
 #save data
-data.to_csv('../data/clean_census.csv', index=False)
+data.to_csv('data/clean_census.csv', index=False)
