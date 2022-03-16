42f7f312d5f229e77ae99d1c44a649418beb69f6
sofiajeron <sofiajdossantos@gmail.com>
2022-03-16 22:53:11 +0000

    remove inference part and save models

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
-from ml.model import train_model, inference, compute_model_metrics, save_model_metrics
+from ml.model import train_model
 import pickle
 
 # Add code to load in the data.
@@ -10,6 +10,9 @@ data = pd.read_csv('data/clean_census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
+
+train.to_csv("data/train.csv", index= False)
+test.to_csv("data/test.csv", index = False)
 
 cat_features = [
     "workclass",
@@ -31,17 +34,7 @@ with open('data/encoder.pickle', 'wb') as f:
 with open('data/lb.pickle', 'wb') as f:
     pickle.dump(lb, f)
 
-# Process the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
-)
 # Train and save a model.
 model = train_model(X_train, y_train)
 
 model.save_model("model/model.json")
-
-preds = inference(model, X_test)
-precision, recall, fbeta = compute_model_metrics(y_test, preds)
-save_model_metrics("precision", precision)
-save_model_metrics("recall", recall)
-save_model_metrics("fbeta", fbeta)
