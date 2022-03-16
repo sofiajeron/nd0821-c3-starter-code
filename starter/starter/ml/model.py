0d420d00f738c036e25aa5d8ca653c7e26f9b683
sofiajeron <sofiajdossantos@gmail.com>
2022-03-16 22:51:17 +0000

    remove evaluations from here

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,6 +1,4 @@
-from sklearn.metrics import fbeta_score, precision_score, recall_score
 import xgboost as xgb
-import json
 
 
 # Optional: implement hyperparameter tuning.
@@ -29,48 +27,7 @@ def train_model(X_train, y_train):
     return model
 
 
-def compute_model_metrics(y, preds):
-    """
-    Validates the trained machine learning model using precision, recall, and F1.
-
-    Inputs
-    ------
-    y : np.array
-        Known labels, binarized.
-    preds : np.array
-        Predicted labels, binarized.
-    Returns
-    -------
-    precision : float
-    recall : float
-    fbeta : float
-    """
-    fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
-    precision = precision_score(y, preds, zero_division=1)
-    recall = recall_score(y, preds, zero_division=1)
-    return precision, recall, fbeta
-
-
-def save_model_metrics(metric_name, metric_value):
-    with open(f'model/{metric_name}.json', 'w') as f:
-        json.dump({metric_name: metric_value}, f)
-
-
-def inference(model, X):
-    """ Run model inferences and return the predictions.
-
-    Inputs
-    ------
-    model : ???
-        Trained machine learning model.
-    X : np.array
-        Data used for prediction.
-    Returns
-    -------
-    preds : np.array
-        Predictions from the model.
-    """
-    X = prepare(X, None)
-    score = model.predict(X)
-    preds = [round(value) for value in score]
-    return preds
+def load_xgb_model(path):
+    model_xgb = xgb.Booster()
+    model_xgb.load_model(path)
+    return model_xgb
