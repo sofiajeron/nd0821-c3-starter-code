30c4fffa06f351e2e951e83341a6bfa9a2f0e308
sofiajeron <sofiajdossantos@gmail.com>
2022-03-16 22:49:36 +0000

    add unit test for evaluation

---
+++ tests/test_evaluation.py
@@ -0,0 +1,17 @@
+from starter.starter.ml.evaluation import compute_model_metrics, inference
+
+
+def test_inference(model, process_data_test_fixture):
+    X_test, _ = process_data_test_fixture
+    preds = inference(model, X_test)
+
+    assert preds.size[0] == X_test.shape[0]
+
+
+def test_compute_model_metrics(process_data_test_fixture, predictions):
+    _, y_test = process_data_test_fixture
+    precision, recall, fbeta = compute_model_metrics(y_test, predictions)
+
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
