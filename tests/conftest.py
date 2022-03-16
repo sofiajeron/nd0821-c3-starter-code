c8d0d27108cfe50a81064d27cc9e00080f7d07b7
sofiajeron <sofiajdossantos@gmail.com>
2022-03-16 22:50:17 +0000

    add conf for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,70 @@
+import pytest
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from starter.starter.ml.data import process_data
+from starter.starter.ml.model import train_model, inference
+
+
+@pytest.fixture
+def data_fixture():
+    return pd.read_csv("data/clean_census.csv")
+
+
+@pytest.fixture
+def train_data_fixture(data):
+    train, _ = train_test_split(data, test_size=0.20)
+    return train
+
+
+@pytest.fixture
+def test_data_fixture(data):
+    _, test = train_test_split(data, test_size=0.20)
+    return test
+
+
+@pytest.fixture
+def cat_features_fixture():
+    cat = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    return cat
+
+
+@pytest.fixture
+def process_data_train_fixture(train_data, cat):
+    X, y, _, _ = process_data(
+        train_data, categorical_features=cat, label="salary", training=True)
+    return X, y
+
+@pytest.fixture
+def encoder_lb_fixture(train_data, cat):
+    _, _, encoder, lb = process_data(
+        train_data, categorical_features=cat, label="salary", training=True)
+    return encoder, lb
+
+@pytest.fixture
+def process_data_test_fixture(test_data, cat, encoder_lb):
+    encoder, lb = encoder_lb
+    X, y, _, _ =  process_data(
+        test_data, categorical_features=cat, label="salary", training=False, encoder=encoder, lb=lb
+    )
+    return X, y
+
+@pytest.fixture
+def train_model(process_data_train_fixture):
+    X_train, y_train = process_data_train_fixture
+    model = train_model(X_train, y_train)
+    return model
+
+@pytest.fixture
+def predictions(model, process_data_test_fixture):
+    X_test, _ = process_data_test_fixture
+    preds = inference(model, X_test)
+    return preds
