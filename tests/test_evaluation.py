4ab95b0490d23965cfe9e1f965aa13822074c312
sofiajeron <sofiajdossantos@gmail.com>
2022-03-17 23:50:20 +0000

    fix preds size

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -5,7 +5,7 @@ def test_inference(train_model_fixture, process_data_test_fixture):
     X_test, _ = process_data_test_fixture
     preds = inference(train_model_fixture, X_test)
 
-    assert preds.size[0] == X_test.shape[0]
+    assert preds.shape[0] == X_test.shape[0]
 
 
 def test_compute_model_metrics(process_data_test_fixture, predictions_fixture):
