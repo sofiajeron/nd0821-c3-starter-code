70c70bd676cced03c4e5b7f19fc2959d20dc11e5
sofiajeron <sofiajdossantos@gmail.com>
2022-03-17 23:50:31 +0000

    fix type of xgboost

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,4 @@
-import xgboost as xgb
+import xgboost
 from starter.ml.model import train_model
 
 
@@ -6,4 +6,4 @@ def test_train_model(process_data_train_fixture):
     X_train, y_train = process_data_train_fixture
     model = train_model(X_train, y_train)
 
-    assert isinstance(model, xgb.Booster())
+    assert isinstance(model, xgboost.core.Booster)
