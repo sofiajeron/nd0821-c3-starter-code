364a8a052bc3487ed8a9218a5d40fd22f45f3896
sofiajeron <sofiajdossantos@gmail.com>
2022-03-19 04:03:56 +0000

    add force

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from starter.starter.ml.model import load_xgb_model
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    os.system("dvc remote add -d s3-bucket s3://nd0821-c3-starter-code")
-    if os.system("dvc pull") != 0:
+    os.system("dvc remote add -d s3-bucket s3://nd0821-c3-starter-code/")
+    if os.system("dvc pull --force") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/libd/dvc")
 
